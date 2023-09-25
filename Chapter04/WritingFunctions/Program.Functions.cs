e06d8d6a3e62feea3bea435c0f60af59284e1822
Max Burrows <maxajburrows@gmail.com>
2023-09-25 21:54:17 +0200

    Write first dotnet function

--- Chapter04/WritingFunctions/Program.Functions.cs
+++ Chapter04/WritingFunctions/Program.Functions.cs
@@ -1,11 +1,12 @@
 partial class Program
 {
-    static void TimeTable(byte number, byte size=12)
+    static void TimesTable(byte number, byte size=12)
     {
         WriteLine($"This is the {number} times table with {size} rows:");
         for (int row=1; row <= size; row++)
         {
-            WriteLine($"")
+            WriteLine($"{row} x {number} = {row*number}");
         }
+        WriteLine();
     }
 }
