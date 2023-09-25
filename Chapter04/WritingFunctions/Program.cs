58fb7eb714ec6d953be3ce46fbaf02956268fa68
Max Burrows <maxajburrows@gmail.com>
2023-09-25 22:04:47 +0200

    Write second dotnet function

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -1,1 +1,3 @@
-﻿TimesTable(number: 7, size: 20);
+﻿//TimesTable(number: 7, size: 20);
+decimal taxToPay = CalculateTax(amount: 149, twoLetterRegionCode: "FR");
+WriteLine($"You must pay {taxToPay} in tax.");
