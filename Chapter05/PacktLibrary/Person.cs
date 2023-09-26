f898f8d63b9e5c9609611a05df546bd7a8d2107d
Max Burrows <maxajburrows@gmail.com>
2023-09-26 18:43:18 +0200

    Using enums

--- Chapter05/PacktLibrary/Person.cs
+++ Chapter05/PacktLibrary/Person.cs
@@ -2,5 +2,8 @@ namespace Packt.Shared;
 
 public class Person: object
 {
-
+    public string? Name;
+    public DateTime DateOfBirth;
+    public WondersOfTheAncientWorld FavouriteAncientWonder;
+    public WondersOfTheAncientWorld BucketList;
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -3,10 +3,18 @@
 Person max = new();
 max.Name = "Max Burrows";
 max.DateOfBirth = new DateTime(1997, 11, 5);
+max.FavouriteAncientWonder = WondersOfTheAncientWorld.StatueOfZeusAtOlympia;
+max.BucketList = 
+    WondersOfTheAncientWorld.HangingGardensOfBabylon
+    | WondersOfTheAncientWorld.MausoleumAtHalicarnassus;
 WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}",
     arg0: max.Name,
     arg1: max.DateOfBirth);
-WriteLine(max.DateOfBirth);
+WriteLine(format: "{0}'s favourite wonder is {1}. Its integer is {2}.",
+    arg0: max.Name,
+    arg1: max.FavouriteAncientWonder,
+    arg2: (int) max.FavouriteAncientWonder);
+WriteLine($"{max.Name}'s bucket list is {max.BucketList}");
 
 Person alice = new()
 {
