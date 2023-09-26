9dd66c04e565896c75bbce9e7f2afc9b0dcdadcb
Max Burrows <maxajburrows@gmail.com>
2023-09-26 18:22:25 +0200

    Instantiate classes

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -1,4 +1,18 @@
 ﻿using Packt.Shared;
 
 Person max = new();
-WriteLine(max.ToString());
+max.Name = "Max Burrows";
+max.DateOfBirth = new DateTime(1997, 11, 5);
+WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}",
+    arg0: max.Name,
+    arg1: max.DateOfBirth);
+WriteLine(max.DateOfBirth);
+
+Person alice = new()
+{
+    Name = "Alice Jones",
+    DateOfBirth = new(1998, 3, 7)
+};
+WriteLine(format: "{0} was born on {1:dd MMM yy}",
+    arg0: alice.Name,
+    arg1: alice.DateOfBirth);
