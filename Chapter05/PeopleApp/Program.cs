c0d0bbe3f2fbbc1157606a84a86e34fcbb2a33a9
Max Burrows <maxajburrows@gmail.com>
2023-09-28 14:52:08 +0200

    Pattern matching

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -161,3 +161,42 @@ for (int i = 0; i < lamech.Children.Count; i++)
 }
 
 WriteLine($"5! is {Person.Factorial(5)}");
+
+Passenger[] passengers = {
+    new FirstClassPassenger { AirMiles = 1_419, Name = "Suman" },
+    new FirstClassPassenger { AirMiles = 16_562, Name = "Lucy" },
+    new BusinessClassPassenger { Name = "Janice" },
+    new CoachClassPassenger { CarryOnKG = 25.7, Name = "Dave" },
+    new CoachClassPassenger { CarryOnKG = 0, Name = "Amit" }
+};
+// foreach (Passenger passenger in passengers)
+// {
+//     decimal flightCost = passenger switch
+//     {
+//         FirstClassPassenger p when p.AirMiles > 3500 => 1500M,
+//         FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
+//         FirstClassPassenger _ => 2000M,
+//         BusinessClassPassenger _ => 1000M,
+//         CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+//         CoachClassPassenger _ => 650M,
+//         _ => 800M
+//     };
+//     WriteLine($"Flight costs {flightCost:C} for {passenger}");
+// }
+foreach (Passenger passenger in passengers)
+{
+    decimal flightCost = passenger switch
+    {
+        FirstClassPassenger p => p.AirMiles switch
+        {
+            > 35000 => 1500M,
+            > 15000 => 1750M,
+            _ => 2000M
+        },
+        BusinessClassPassenger => 1000M,
+        CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+        CoachClassPassenger => 650M,
+        _ => 800M
+    };
+    WriteLine($"Flight costs {flightCost:C} for {passenger}");
+}
