a2d02399024f7037261a33aa26c5c2fb25c15aef
Max Burrows <maxajburrows@gmail.com>
2023-10-02 23:19:40 +0200

    Submit new entity

---
+++ Chapter10/WorkingWithEFCore/Program.Modifications.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Packt.Shared;
+
+partial class Program
+{
+    static void ListProducts(int? productIdToHighlight = null)
+    {
+        using (Northwind db = new())
+        {
+            if ((db.Products is null) || (db.Products.Count() == 0))
+            {
+                Fail("There are no products.");
+                return;
+            }
+            WriteLine("| {0,-3} | {1,-35} | {2,8} | {3,5} | {4} |", "Id", "Product Name", "Cost", "Stock", "Disc.");
+            foreach (Product p in db.Products)
+            {
+                ConsoleColor previousColor = ForegroundColor;
+                if (productIdToHighlight == p.ProductId)
+                {
+                    ForegroundColor = ConsoleColor.Yellow;
+                }
+                WriteLine("| {0:000} | {1,-35} | {2,8:$#,##0.00} | {3,5} | {4} |", p.ProductId, p.ProductName, p.Cost, p.Stock, p.Discontinued);
+                ForegroundColor = previousColor;
+            }
+        }
+    }
+
+    static (int affected, int productId) AddProduct(int categoryId, string productName, decimal? price)
+    {
+        using (Northwind db = new())
+        {
+            Product p = new()
+            {
+                CategoryId = categoryId,
+                ProductName = productName,
+                Cost = price,
+                Stock = 72
+            };
+        EntityEntry<Product> entity = db.Products.Add(p);
+        WriteLine($"State: {entity.State}, ProductId: {p.ProductId}");
+        int affected = db.SaveChanges();
+        WriteLine($"State: {entity.State}, ProductId: {p.ProductId}");
+        return (affected, p.ProductId);
+        }
+    }
+}
--- Chapter10/WorkingWithEFCore/Program.cs
+++ Chapter10/WorkingWithEFCore/Program.cs
@@ -1,7 +1,13 @@
 ﻿using Packt.Shared;
 
-QueryingCategories();
+// QueryingCategories();
 // FilteredIncludes();
 // QueryingProducts();
 // QueryingWithLike();
 //GetRandomProduct();
+// var resultAdd = AddProduct(categoryId: 6, productName: "Super Seafood", price: 700M);
+// if (resultAdd.affected == 1)
+// {
+//     WriteLine("Add product successful.");
+// }
+// ListProducts(productIdToHighlight: resultAdd.productId);
