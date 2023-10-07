94aa773074900cb03562a5fd721475a8ab530721
Max Burrows <maxajburrows@gmail.com>
2023-10-07 19:26:20 +0200

    Create database from scratch using EF Core

--- Chapter10/CoursesAndStudents/Academy.cs
+++ Chapter10/CoursesAndStudents/Academy.cs
@@ -6,4 +6,38 @@ public class Academy : DbContext
 {
     public DbSet<Student>? Students { get; set; }
     public DbSet<Course>? Courses { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        string path = Path.Combine(Environment.CurrentDirectory, "Academy.db");
+        string connection = $"Filename={path}";
+        Console.WriteLine($"Connection: {connection}");
+        optionsBuilder.UseSqlite(connection);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>()
+            .Property(s => s.LastName).HasMaxLength(30).IsRequired();
+
+        Student alice = new() { StudentId = 1, FirstName = "Alice", LastName = "Jones" };
+        Student bob = new() { StudentId = 2, FirstName = "Bob", LastName = "Smith" };
+        Student cecilia = new() { StudentId = 3, FirstName = "Cecilia", LastName = "Ramirez" };
+        Course csharp = new() { CourseId = 1, Title = "C# 11 and .NET 7" };
+        Course webdev = new() { CourseId = 2, Title = "Web Development" };
+        Course python = new() { CourseId = 3, Title = "Python for Beginners" };
+
+        modelBuilder.Entity<Student>()
+            .HasData(alice, bob, cecilia);
+        modelBuilder.Entity<Course>()
+            .HasData(csharp, webdev, python);
+        modelBuilder.Entity<Course>()
+            .HasMany(c => c.Students)
+            .WithMany(s => s.Courses)
+            .UsingEntity(e => e.HasData(
+                new { CoursesCourseId = 1, StudentsStudentId = 1 },
+                new { CoursesCourseId = 1, StudentsStudentId = 2 },
+                new { CoursesCourseId = 1, StudentsStudentId = 3 },
+                new { CoursesCourseId = 2, StudentsStudentId = 2 },
+                new { CoursesCourseId = 3, StudentsStudentId = 3 }
+            ));
+    }
 }
--- Chapter10/CoursesAndStudents/Program.cs
+++ Chapter10/CoursesAndStudents/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+
+using Packt.Shared;
+
+using (Academy a = new())
+{
+    bool deleted = await a.Database.EnsureDeletedAsync();
+    Console.WriteLine($"Database deleted: {deleted}");
+    bool created = await a.Database.EnsureCreatedAsync();
+    Console.WriteLine($"Database created: {created}");
+    Console.WriteLine("SQL script used to create database:");
+    Console.WriteLine(a.Database.GenerateCreateScript());
+
+    
+    foreach (Student s in a.Students.Include(s => s.Courses))
+    {
+        Console.WriteLine("{0} {1} attends the following {2} courses:",
+            s.FirstName, s.LastName, s.Courses.Count);
+        foreach (Course c in s.Courses)
+        {
+            Console.WriteLine($" {c.Title}");
+        }
+    }
+
+}
