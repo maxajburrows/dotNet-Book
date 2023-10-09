b74560ec47debe9c21525365c0ce71c5df8acc1d
Max Burrows <maxajburrows@gmail.com>
2023-10-09 13:43:41 +0200

    Use set operations

--- Chapter11/LinqWithObjects/Program.Functions.cs
+++ Chapter11/LinqWithObjects/Program.Functions.cs
@@ -1,7 +1,21 @@
+using static System.Console;
+
+
 partial class Program
 {
     static bool NameLongerThanFour(string name)
     {
         return name.Length > 4;
+    }
+
+    static void Output(IEnumerable<string> cohort, string description = "")
+    {
+        if (!string.IsNullOrEmpty(description))
+        {
+            WriteLine(description);
+        }
+        Write(" ");
+    WriteLine(string.Join(", ", cohort.ToArray()));
+    WriteLine();
     }
 }
--- Chapter11/LinqWithObjects/Program.cs
+++ Chapter11/LinqWithObjects/Program.cs
@@ -17,7 +17,7 @@ foreach (string name in query1)
 
 SectionTitle("Writing queries");
 //var query = names.Where(new Func<string, bool>(NameLongerThanFour));
-var query = names
+IOrderedEnumerable<string> query = names
     .Where(name => name.Length > 4)
     .OrderBy(name => name.Length)
     .ThenBy(name => name);
@@ -25,3 +25,41 @@ foreach (string item in query)
 {
     WriteLine(item);
 }
+
+SectionTitle("Filtering by type");
+List<Exception> exceptions = new()
+{
+    new ArgumentException(),
+    new SystemException(),
+    new IndexOutOfRangeException(),
+    new InvalidOperationException(),
+    new NullReferenceException(),
+    new InvalidCastException(),
+    new OverflowException(),
+    new DivideByZeroException(),
+    new ApplicationException()
+};
+
+IEnumerable<ArithmeticException> arithmeticExceptionsQuery = 
+    exceptions.OfType<ArithmeticException>();
+
+foreach (ArithmeticException exception in arithmeticExceptionsQuery)
+{
+    WriteLine(exception);
+}
+
+string[] cohort1 = new[] { "Rachel", "Gareth", "Jonathan", "George" };
+string[] cohort2 = new[] { "Jack", "Stephen", "Daniel", "Jack", "Jared" };
+string[] cohort3 = new[] { "Declan", "Jack", "Jack", "Jasmine", "Conor" };
+SectionTitle("The cohorts");
+Output(cohort1, "Cohort 1");
+Output(cohort2, "Cohort 2");
+Output(cohort3, "Cohort 3");
+SectionTitle("Set operations");
+Output(cohort2.Distinct(), "cohort2.Distinct()");
+Output(cohort2.DistinctBy(name => name.Substring(0, 2)), "cohort2.DistinctBy(name => name.Substring(0, 2)):");
+Output(cohort2.Union(cohort3), "cohort2.Union(cohort3)");
+Output(cohort2.Concat(cohort3), "cohort2.Concat(cohort3)");
+Output(cohort2.Intersect(cohort3), "cohort2.Intersect(cohort3)");
+Output(cohort2.Except(cohort3), "cohort2.Except(cohort3)");
+Output(cohort1.Zip(cohort2,(c1, c2) => $"{c1} matched with {c2}"), "cohort1.Zip(cohort2)");
