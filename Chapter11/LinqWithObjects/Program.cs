71faae00268bb475fabd67ebe43ccebf85f8022b
Max Burrows <maxajburrows@gmail.com>
2023-10-09 11:53:16 +0200

    Filter entities using Where

--- Chapter11/LinqWithObjects/Program.cs
+++ Chapter11/LinqWithObjects/Program.cs
@@ -1,13 +1,23 @@
-﻿string[] names = new[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed"};
+﻿using static System.Console;
+
+string[] names = new[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed"};
 SectionTitle("Deferred execution");
 var query1 = names.Where(name => name.EndsWith("m"));
 var query2 = from name in names where name.EndsWith("m") select name;
 string[] result1 = query1.ToArray();
 List<string> result2 = query2.ToList();
-Console.WriteLine(result1.ToString());
-Console.WriteLine(result2.ToString());
+WriteLine(result1.ToString());
+WriteLine(result2.ToString());
+
 foreach (string name in query1)
 {
-    Console.WriteLine(name);
+    WriteLine(name);
     names[2] = "Jimmy";
+}
+
+SectionTitle("Writing queries");
+var query = names.Where(new Func<string, bool>(NameLongerThanFour));
+foreach (string item in query)
+{
+    WriteLine(item);
 }
