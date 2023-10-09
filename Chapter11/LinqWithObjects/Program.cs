3f2386f7f8c2e5639c2f63078428c9bd51a36379
Max Burrows <maxajburrows@gmail.com>
2023-10-09 10:24:34 +0200

    Testing deferred execution with LINQ

---
+++ Chapter11/LinqWithObjects/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] names = new[] { "Michael", "Pam", "Jim", "Dwight", "Angela", "Kevin", "Toby", "Creed"};
+SectionTitle("Deferred execution");
+var query1 = names.Where(name => name.EndsWith("m"));
+var query2 = from name in names where name.EndsWith("m") select name;
+string[] result1 = query1.ToArray();
+List<string> result2 = query2.ToList();
+Console.WriteLine(result1.ToString());
+Console.WriteLine(result2.ToString());
+foreach (string name in query1)
+{
+    Console.WriteLine(name);
+    names[2] = "Jimmy";
+}
