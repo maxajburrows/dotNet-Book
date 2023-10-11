cf5d62b846b8c1a116dff2195275c5108e9dc680
Max Burrows <maxajburrows@gmail.com>
2023-10-11 17:28:30 +0200

    Understanding relationship between C# and dotnet

---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Linq;  // XDocument
+using System;  // String
+
+XDocument doc = new();
+
+string s1 = "Hello";
+String s2 = "World";
+WriteLine($"{s1} {s2}");
+
+WriteLine($"int.MaxValue = {int.MaxValue:N0}");
+WriteLine($"nint.MaxValue = {nint.MaxValue:N0}");
