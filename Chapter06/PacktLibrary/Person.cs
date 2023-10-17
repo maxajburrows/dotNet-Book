2166718c11b03bc5f2d0c735d834a488d6d8bc6c
Max Burrows <maxajburrows@gmail.com>
2023-10-17 18:45:13 +0200

    Implement interfaces

--- Chapter06/PacktLibrary/Person.cs
+++ Chapter06/PacktLibrary/Person.cs
@@ -1,6 +1,6 @@
 ﻿namespace Packt.Shared;
 
-public class Person : object
+public class Person : object, IComparable<Person?>
 {
     public string? Name { get; set; }
     public DateTime DateOfBirth { get; set; }
@@ -17,5 +17,42 @@ public class Person : object
         {
             Shout?.Invoke(this, EventArgs.Empty);
         }
+    }
+
+    public int CompareTo(Person? other)
+    {
+        int position;
+        if ((this is not null) && (other is not null))
+        {
+            if ((Name is not null) && (other.Name is not null))
+            {
+                position = Name.CompareTo(other.Name);
+            }
+            else if ((Name is not null) && (other.Name is null))
+            {
+                position = -1;
+            }
+            else if ((Name is null) && (other.Name is not null))
+            {
+                position = 1;
+            } 
+            else 
+            {
+                position = 0;
+            }
+        }
+        else if ((this is not null) && (other is null))
+        {
+            position = -1;
+        }
+        else if ((this is null) && (other is not null))
+        {
+            position = 1;
+        }
+        else
+        {
+            position = 0;
+        }
+        return position;    
     }
 }
